1236086480f2d2781148b06452493bbb37620ada
Rafagons <rafaagons@gmail.com>
2024-05-28 12:09:29 -0300

    Codigo pronto.

--- src/Contador.java
+++ src/Contador.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Contador {
 	public static void main(String[] args) {
         Scanner terminal = new Scanner (System.in);
-        System.out.println("|=======Olá, Bem Vindo!=======|");
+        System.out.println("  Seja Bem Vindo!  ");
         System.out.println("Digite o primeiro parâmetro");
         int parametroUm = terminal.nextInt();
         System.out.println("Digite o segundo parâmetro");
